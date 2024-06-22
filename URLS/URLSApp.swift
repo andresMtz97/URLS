f56e0d9b0327f3091bfcf6875d16df080105f981
Andrés Martínez <andres.martinez@proveeduria.unam.mx>
2024-06-21 19:55:15 -0600

    Initial Commit

---
+++ URLS/URLSApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSApp.swift
+//  URLS
+//
+//  Created by DISMOV on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct URLSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
