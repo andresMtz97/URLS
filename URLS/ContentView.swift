b1cbb97497a8437110c706a4aefdc70013a29525
Andrés Martínez <andres.martinez@proveeduria.unam.mx>
2024-06-24 17:29:05 -0600

    urls

--- URLS/ContentView.swift
+++ URLS/ContentView.swift
@@ -6,14 +6,51 @@
 //
 
 import SwiftUI
+import Awesome
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                print("tap")
+                let urlString = "fb://events/2111475325889787"
+                if let url = URL(string: urlString) {
+                    if UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url)
+                    }
+                }
+            } label: {
+                Awesome.Brand.facebookSquare.image
+                    .size(45)
+                    .foregroundColor(.blue)
+                    .backgroundColor(.white)
+                Text("facebook")
+                    
+                    .padding(.vertical, 10)
+            }
+            .padding(.horizontal, 20)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            
+            Button {
+                print("tap settings")
+                let urlString = UIApplication.openSettingsURLString 
+                if let url = URL(string: urlString) {
+                    if UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url)
+                    }
+                }
+            } label: {
+                Image(systemName: "gear")
+                Text("Settings")
+                    
+                    .padding(.vertical, 10)
+            }
+            .padding(.horizontal, 20)
+            .background(.gray)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
         }
         .padding()
     }
